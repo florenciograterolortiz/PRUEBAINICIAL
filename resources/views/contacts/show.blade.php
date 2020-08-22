92ca5e78974df7014473748e6a151440569ec933
florencio graterol <inggraterol76@gmail.com>
2020-08-22 00:16:44 -0400

    cambios a  la vista show

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -12,7 +12,10 @@
               <h2 class="card-subtitle  card mb-4" >{{$contact->apellido}}</h2>
               <h3 class="card-subtitle  card mb-4 " >{{$contact->email}}</h3>
               <h4 class="card-title card mb-4 " >{{$contact->numbercontact}}</h4>
-           
+                             
+               <div class="editbotom">
+               <button class="button-with-arrow" style="padding: 8px 15px 8px 15px; font-size: 17px;margin-top: 15px;"><a href="{{ route('contacts.edit', $contact->id)  }}">Crear Registro</a></button>
+               </div>
           @include('contacts.delete');
             </div>
           </div>
