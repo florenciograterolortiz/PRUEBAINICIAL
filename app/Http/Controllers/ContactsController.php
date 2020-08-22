86fb53374b61005b82327b0cb9c5cefb4e8cf2fc
florencio graterol <inggraterol76@gmail.com>
2020-08-21 23:56:12 -0400

    desarrollo del dia 21

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Contact;
+use Illuminate\Http\Request;
+
+class ContactsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+
+       return view('contacts.index', ['contacts' => $contacts]);
+         }
+
+    public function create()
+    {  
+     $contact = new  Contact;
+       return view('contacts.create', ['contact' => $contact]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       $options = [
+          'name'  => $request->nombre,
+           'apellido'  => $request->apellido,
+           'email'  => $request->correo,
+            'numbercontact'  => $request->numbercontact
+       ];
+
+       if(Contact::create($options)) {
+
+       	return redirect('/');
+      
+       }else{
+
+       	return view('contacts.create');
+       }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = Contact::find($id);
+      return view("contacts.show",['contact' => $contact]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      $contact = Contact::find($id);
+      return view("contacts.edit",['contact' => $contact]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+            $contact = Contact::find($id);
+
+            $contact->name =  $request->nombre;
+            $contact->apellido =  $request->apellido;
+            $contact->email =  $request->correo;
+            $contact->numbercontact =  $request->numbercontact;
+
+            if($contact->save()){
+
+                return redirect('/');
+            }else{
+
+      	return view('contacts.create');
+       }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       Contact::destroy($id);
+      return redirect('/contacts');
+    }
+}
---
+++ resources/views/contacts/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+<div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card mb-4 shadow-sm">
+          
+            <div class="card-body">
+              <h1 class="card-title card mb-4 " >{{$contact->name}}</h1>
+              <h2 class="card-subtitle  card mb-4" >{{$contact->apellido}}</h2>
+              <h3 class="card-subtitle  card mb-4 " >{{$contact->email}}</h3>
+              <h4 class="card-title card mb-4 " >{{$contact->numbercontact}}</h4>
+           
+          @include('contacts.delete');
+            </div>
+          </div>
+        </div>
+     
+      
+      </div>
+    </div>
+  </div>
+  @endsection
